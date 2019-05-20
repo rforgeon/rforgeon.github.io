bbb98113f2470f9651f4e2faafd7012652d6b68b
Ralph <rforgeon@gmail.com>
2019-05-20 22:25:14 +0100

    Add files via upload

---
+++ precache-manifest.a1d9da38af3fee4b5d18114d6e2eae51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11982cdb976cf9b65e1a67b2d4de259d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a185923f53d9a834c28c",
+    "url": "/static/css/main.4b1400d6.chunk.css"
+  },
+  {
+    "revision": "f5aa4af3615738118eab",
+    "url": "/static/js/2.ac968b21.chunk.js"
+  },
+  {
+    "revision": "a185923f53d9a834c28c",
+    "url": "/static/js/main.d807ae26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
